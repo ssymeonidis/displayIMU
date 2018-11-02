5ec38734b82d4cc9ae1d33764623621edd8ac8e9
simeon symeonidis <simeon.symeonidis@l3t.com>
2018-11-01 21:42:33 -0400

    added basic statistics to IMU_stat

--- imu/IMU_stat.c
+++ imu/IMU_stat.c
@@ -18,12 +18,18 @@
 */
 
 // include statements 
+#include <math.h>
 #include "IMU_stat.h"
 
 // internally managed structures
-static IMU_stat_config  config [IMU_MAX_INST]; 
-static IMU_stat_state   state  [IMU_MAX_INST];
-static uint16_t         numInst = 0;
+static IMU_stat_config    config [IMU_MAX_INST]; 
+static IMU_stat_state     state  [IMU_MAX_INST];
+static uint16_t           numInst = 0;
+
+// internal functions definitions
+int IMU_stat_gyro (uint16_t id, uint32_t t, IMU_TYPE *g, IMU_core_FOM*);
+int IMU_stat_accl (uint16_t id, uint32_t t, IMU_TYPE *a, IMU_core_FOM*);
+int IMU_stat_magn (uint16_t id, uint32_t t, IMU_TYPE *m, IMU_core_FOM*);
 
 
 /******************************************************************************
@@ -88,16 +94,29 @@ int IMU_stat_getState(
 
 
 /******************************************************************************
-* process gyroscope, accelerometer, and magnetometer vectors
+* initialize instance to known state
 ******************************************************************************/
 
 int IMU_stat_reset(
   uint16_t                id)
 {
-  state[id].gBias         = 0.0f;
+  // check out-of-bounds condition
+  if (id >= numInst)
+    return IMU_STAT_BAD_INST; 
+
+  // initialize instantce
+  state[id].gBias[0]      = 0.0f;
+  state[id].gBias[1]      = 0.0f;
+  state[id].gBias[2]      = 0.0f;
+  state[id].gBiasStd[0]   = 0.0f;
+  state[id].gBiasStd[1]   = 0.0f;
+  state[id].gBiasStd[2]   = 0.0f;
   state[id].aMag          = 0.0f;
+  state[id].aMagStd       = 0.0f;
   state[id].mMag          = 0.0f;
+  state[id].mMagStd       = 0.0f;
   state[id].mDot          = 0.0f;
+  state[id].mDotStd       = 0.0f;
   state[id].gClock        = 1;
   state[id].aClock        = 1;
   state[id].mClock        = 1;
@@ -105,7 +124,7 @@ int IMU_stat_reset(
   state[id].tAccl         = 0;
   state[id].tMagn         = 0;
 
-  // exit function (no failures)
+  // exit function (no errors)
   return 0;
 }
 
@@ -119,6 +138,21 @@ int IMU_stat_datum(
   IMU_datum               *datum, 
   IMU_core_FOM            *FOM)
 {
+  // check out-of-bounds condition
+  if (id >= numInst)
+    return IMU_STAT_BAD_INST; 
+  if (!config[id].enable)
+    return IMU_STAT_FNC_DISABLED;
+
+  // check sensor type and execute
+  if      (datum->type == IMU_gyro)
+    IMU_stat_gyro(id, datum->t, datum->val, FOM);
+  else if (datum->type == IMU_accl)
+    IMU_stat_accl(id, datum->t, datum->val, FOM);
+  else if (datum->type == IMU_magn)
+    IMU_stat_magn(id, datum->t, datum->val, FOM);
+
+  // exit function (no errors)
   return 0;
 }
 
@@ -132,5 +166,128 @@ int IMU_stat_data3(
   IMU_data3               *data3, 
   IMU_core_FOM            *FOM)
 {
+  // check out-of-bounds condition
+  if (id >= numInst)
+    return IMU_STAT_BAD_INST; 
+  if (!config[id].enable)
+    return IMU_STAT_FNC_DISABLED;
+
+  // check sensor type and execute
+  IMU_stat_gyro(id, data3->t, data3->g, &FOM[0]);
+  IMU_stat_accl(id, data3->t, data3->a, &FOM[1]);
+  IMU_stat_magn(id, data3->t, data3->m, &FOM[2]);
+
+  // exit function (no errors)
+  return 0;
+}
+
+
+/******************************************************************************
+* collect gyroscope statistics
+******************************************************************************/
+
+int IMU_stat_gyro(
+  uint16_t                id, 
+  uint32_t                t, 
+  IMU_TYPE                *g,
+  IMU_core_FOM            *FOM)
+{
+  // first sensor datum
+  if (state[id].gClock) {
+    state[id].gBias[0]    = g[0];
+    state[id].gBias[1]    = g[1];
+    state[id].gBias[2]    = g[2];
+    state[id].gClock      = 0;
+
+  // nominal condition
+  } else {
+    float dt              = (t-state[id].tGyro) * IMU_STAT_10USEC_TO_SEC;
+    float alpha           = dt * config[id].alpha;
+    float *gBias          = state[id].gBias;
+    float *prev           = state[id].gBiasStd;
+    state[id].gBiasStd[0] = (1.0f-alpha)*prev[0] + alpha*fabs(gBias[0]-g[0]); 
+    state[id].gBiasStd[1] = (1.0f-alpha)*prev[1] + alpha*fabs(gBias[1]-g[1]); 
+    state[id].gBiasStd[2] = (1.0f-alpha)*prev[2] + alpha*fabs(gBias[2]-g[2]); 
+    state[id].gBias[0]    = (1.0f-alpha)*gBias[0] + alpha*g[0];
+    state[id].gBias[1]    = (1.0f-alpha)*gBias[1] + alpha*g[1];
+    state[id].gBias[2]    = (1.0f-alpha)*gBias[2] + alpha*g[2];
+  }
+
+  // exit function (no errors)
+  state[id].tGyro       = t;
+  return 0;
+}
+
+
+/******************************************************************************
+* collect accelerometer statistics
+******************************************************************************/
+
+int IMU_stat_accl(
+  uint16_t                id, 
+  uint32_t                t, 
+  IMU_TYPE                *a,
+  IMU_core_FOM            *FOM)
+{
+  // first sensor datum
+  if (state[id].gClock) {
+    state[id].aMag        = FOM->FOM.accl.mag;
+    state[id].aMagFOM     = FOM->FOM.accl.magFOM;
+    state[id].gClock      = 0;
+
+  // nominal condition
+  } else {
+    float dt              = (t-state[id].tGyro) * IMU_STAT_10USEC_TO_SEC;
+    float alpha           = dt * config[id].alpha;
+    float prevMag         = state[id].aMag;
+    float prevStd         = state[id].aMagStd;
+    float prevFOM         = state[id].aMagFOM;
+    float curMag          = FOM->FOM.accl.mag;
+    float curFOM          = FOM->FOM.accl.magFOM;
+    state[id].aMagStd     = (1.0f-alpha)*prevStd + alpha*fabs(prevMag-curMag);
+    state[id].aMag        = (1.0f-alpha)*prevMag + alpha*curMag;
+    state[id].aMagFOM     = (1.0f-alpha)*prevFOM + alpha*curFOM;
+  }
+
+  // exit function (no errors)
+  state[id].tGyro       = t;
+  return 0;
+}
+
+
+/******************************************************************************
+* collect accelerometer statistics
+******************************************************************************/
+
+int IMU_stat_magn(
+  uint16_t                id, 
+  uint32_t                t, 
+  IMU_TYPE                *m,
+  IMU_core_FOM            *FOM)
+{
+  // first sensor datum
+  if (state[id].mClock) {
+    state[id].mMag        = FOM->FOM.magn.mag;
+    state[id].mMagFOM     = FOM->FOM.magn.magFOM;
+    state[id].mDot        = FOM->FOM.magn.dot;
+    state[id].mDotFOM     = FOM->FOM.magn.dotFOM;
+    state[id].mClock      = 0;
+
+  // nominal condition
+  } else {
+    float dt              = (t-state[id].tMagn) * IMU_STAT_10USEC_TO_SEC;
+    float alpha           = dt * config[id].alpha;
+    float prevMag         = state[id].mMag;
+    float prevStd         = state[id].mMagStd;
+    float prevFOM         = state[id].mMagFOM;
+    float curMag          = FOM->FOM.magn.mag;
+    float curFOM          = FOM->FOM.magn.magFOM;
+    state[id].mMagStd     = (1.0f-alpha)*prevStd + alpha*fabs(prevMag-curMag);
+    state[id].mMag        = (1.0f-alpha)*prevMag + alpha*curMag;
+    state[id].mMagFOM     = (1.0f-alpha)*prevFOM + alpha*curFOM;
+  }
+
+  // exit function (no errors)
+  state[id].tGyro       = t;
   return 0;
 }
--- imu/IMU_stat.h
+++ imu/IMU_stat.h
@@ -28,41 +28,53 @@ extern "C" {
 #include <stdint.h>
 #include "IMU_core.h"
 
-// define error codes
-#define IMU_STAT_INST_OVERFLOW  -1
-#define IMU_STAT_BAD_INST	-2
+// define status codes
+#define IMU_STAT_FNC_DISABLED       1
 
+// define error codes
+#define IMU_STAT_INST_OVERFLOW      -1
+#define IMU_STAT_BAD_INST	    -2
+
+// define constants
+#define IMU_STAT_10USEC_TO_SEC      0.00001
 
 // define configuration structure
 typedef struct {
-  unsigned char          enable;          // enable system characterization
-  float                  alpha;           // mean/std calc filter value 
+  unsigned char         enable;          // enable system characterization
+  float                 alpha;           // mean/std calc filter value 
 } IMU_stat_config;
 
 // define internal state 
 typedef struct {
-  float                  gBias;
-  float                  aMag;
-  float                  mMag;
-  float                  mDot;
-  uint8_t                gClock;
-  uint8_t                aClock;
-  uint8_t                mClock;
-  uint32_t               tGyro;
-  uint32_t               tAccl;
-  uint32_t               tMagn;
+  float                 gBias[3];
+  float                 gBiasStd[3];
+  float                 aMag;
+  float                 aMagFOM;
+  float                 aMagStd;
+  float                 mMag;
+  float                 mMagFOM;
+  float                 mMagStd;
+  float                 mDot;
+  float                 mDotFOM;
+  float                 mDotStd;
+  uint8_t               gClock;
+  uint8_t               aClock;
+  uint8_t               mClock;
+  uint32_t              tGyro;
+  uint32_t              tAccl;
+  uint32_t              tMagn;
 } IMU_stat_state;
 
 
 // data structure access function
-int IMU_stat_init     (uint16_t *id, IMU_stat_config **config);
-int IMU_stat_getConfig (uint16_t id, IMU_stat_config **config);
-int IMU_stat_getState  (uint16_t id, IMU_stat_state  **state);
+int IMU_stat_init      (uint16_t *id, IMU_stat_config **config);
+int IMU_stat_getConfig  (uint16_t id, IMU_stat_config **config);
+int IMU_stat_getState   (uint16_t id, IMU_stat_state  **state);
 
 // general operation functions 
-int IMU_stat_reset     (uint16_t id);
-int IMU_stat_datum     (uint16_t id, IMU_datum*, IMU_core_FOM*);
-int IMU_stat_data3     (uint16_t id, IMU_data3*, IMU_core_FOM*);
+int IMU_stat_reset      (uint16_t id);
+int IMU_stat_datum      (uint16_t id, IMU_datum*, IMU_core_FOM*);
+int IMU_stat_data3      (uint16_t id, IMU_data3*, IMU_core_FOM*);
 
 
 #ifdef __cplusplus
